a0510a1f0f9464d6d79770f510e6e3433909c9ec
Tom Engel <tomengel@outlook.com>
2015-04-25 19:35:22 +0200

    First version of script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# website-minification
+# minify.ps1
 A PowerShell script to minify typical website-projects with html, CSS and JavaScript-files.
+
+For html compression [htmlcompressor](https://code.google.com/p/htmlcompressor/) is used.
+
+For CSS and JavaScript compression [YUI Compressor](http://yui.github.io/yuicompressor/) is used.
+
+These .jars are not included. Make sure you download them and specify the path in the script.
---
+++ minify.ps1
@@ -0,0 +1,71 @@
+﻿#minify.ps1
+#From github.com/tom-engel/website-minifier/
+#Copyright (c) 2015 tom-engel under MIT-License
+#See LICENSE for further details.
+#
+#Minification of html, CSS and JavaScript-files using
+#htmlcompressor [https://code.google.com/p/htmlcompressor/]
+#and
+#yuicompressor [http://yui.github.io/yuicompressor/]
+
+#Path to htmlcompressor
+$html = "C:\jars\htmlcompressor-x.x.x.jar"
+
+#Path to yuicompressor
+$yui = "C:\jars\yuicompressor-x.x.x.jar"
+
+#Path to output folder
+$out ="./min"
+
+#Paths to html-containing folders
+$htmlFolders = "./"
+
+#Paths to CSS-containing folders
+$cssFolders = "./css"
+
+#Paths to js-containing folders
+$jsFolders = "./js"
+
+#Paths to CSS-files
+$cssFiles = "$cssFolders/style.css"
+
+#Paths to js-Files
+$jsFiles = "$jsFolders/script.js"
+
+#Check if out-folders exist. If not, create them
+if (-Not (Test-Path $out)) {
+    mkdir $out
+}
+
+foreach($folder in $htmlFolders) {
+    if (-Not (Test-Path $out/$folder)) {
+        mkdir $out/$folder
+    }
+}
+
+foreach($folder in $cssFolders) {
+    if (-Not (Test-Path $out/$folder)) {
+        mkdir $out/$folder
+    }
+}
+
+foreach($folder in $jsFolders) {
+    if (-Not (Test-Path $out/$folder)) {
+        mkdir $out/$folder
+    }
+}
+
+# Compress HTML
+foreach($folder in $htmlFolders) {
+    java -jar $html $folder -o $out/$folder
+}
+
+# Compress CSS
+foreach($file in $cssFiles) {
+    java -jar $yui -o $out/$file $file
+}
+
+# Compress JavaScript
+foreach($file in $jsFiles) {
+    java -jar $yui -o $out/$file $file
+}

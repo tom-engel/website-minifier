124044605d1709ba773c9a2cb29949d38764bc0b
Tom Engel <tomengel@outlook.com>
2015-04-28 16:53:47 +0200

    Checking folders is now a function

--- minify.ps1
+++ minify.ps1
@@ -1,59 +1,60 @@
-﻿#minify.ps1
-#From github.com/tom-engel/website-minifier/
-#Copyright (c) 2015 tom-engel under MIT-License
-#See LICENSE for further details.
+﻿# minify.ps1
+# From github.com/tom-engel/website-minifier/
+# Copyright (c) 2015 tom-engel under MIT-License
+# See LICENSE for further details.
 #
-#Minification of html, CSS and JavaScript-files using
-#htmlcompressor [https://code.google.com/p/htmlcompressor/]
-#and
-#yuicompressor [http://yui.github.io/yuicompressor/]
+# Minification of html, CSS and JavaScript-files using
+# htmlcompressor [https://code.google.com/p/htmlcompressor/]
+# and
+# yuicompressor [http://yui.github.io/yuicompressor/]
 
-#Path to htmlcompressor
+# Variables
+# Path to htmlcompressor
 $html = "C:\jars\htmlcompressor-x.x.x.jar"
 
-#Path to yuicompressor
+# Path to yuicompressor
 $yui = "C:\jars\yuicompressor-x.x.x.jar"
 
-#Path to output folder
+# Path to output folder
 $out ="./min"
 
-#Paths to html-containing folders
+# Paths to html-containing folders
 $htmlFolders = "./"
 
-#Paths to CSS-containing folders
+# Paths to CSS-containing folders
 $cssFolders = "./css"
 
-#Paths to js-containing folders
+# Paths to js-containing folders
 $jsFolders = "./js"
 
-#Paths to CSS-files
+# Paths to CSS-files
 $cssFiles = "$cssFolders/style.css"
 
-#Paths to js-Files
+# Paths to js-Files
 $jsFiles = "$jsFolders/script.js"
 
-#Check if out-folders exist. If not, create them
+# Functions
+# Check if out-folders exist. If not, create them
+# IN: $folders: Array with folders / folder
+# IN: $out: path to output folder
+Function CheckFolders($folders, $out) {
+
+  foreach($folder in $folders) {
+    if (-Not (Test-Path $out/$folder)) {
+        mkdir $out/$folder
+    }
+  }
+}
+
+# Check out-folder
 if (-Not (Test-Path $out)) {
     mkdir $out
 }
 
-foreach($folder in $htmlFolders) {
-    if (-Not (Test-Path $out/$folder)) {
-        mkdir $out/$folder
-    }
-}
-
-foreach($folder in $cssFolders) {
-    if (-Not (Test-Path $out/$folder)) {
-        mkdir $out/$folder
-    }
-}
-
-foreach($folder in $jsFolders) {
-    if (-Not (Test-Path $out/$folder)) {
-        mkdir $out/$folder
-    }
-}
+# Check other folders
+CheckFolders $htmlFolders $out
+CheckFolders $cssFolders  $out
+CheckFolders $jsFolders   $out
 
 # Compress HTML
 foreach($folder in $htmlFolders) {
